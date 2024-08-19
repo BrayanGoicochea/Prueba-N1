266ae4109cf875b2dcac8ca6525706ee0b6bf93d
Brayan Goicochea <137005551+Emir62004@users.noreply.github.com>
2024-08-19 17:17:53 -0500

    Subiendo el primer archivo

---
+++ 2_Herencia.py
@@ -0,0 +1,32 @@
+class Vehiculo:
+    def __init__(self,marca,modelo,precio,Npasajeros):
+        self.marca = marca
+        self.modelo = modelo
+        self.precio = precio
+        self.Npasajeros = Npasajeros
+
+    def Imprimir(self):
+        print(f"""
+        > Marca = {self.marca}
+        > Modelo = {self.modelo}
+        > Precio = {self.precio}
+        > Numero de pasajeros = {self.Npasajeros}
+        """)
+
+
+class SUV(Vehiculo): #así se indica herencia
+    def __init__(self,marca,modelo,precio,Npasajeros,tracción,Alta): #todos los atributos
+        super().__init__(marca,modelo,precio,Npasajeros)
+        self.traccion=tracción
+        self.Alta=Alta
+
+    def Imprimir(self):
+        super().Imprimir() #acá llamo al método de la clase padre de nombre Imprimir
+        print(f"""
+        > Tracción = {self.traccion}
+        > Npasajeros = {self.Npasajeros}
+        """)
+
+
+carro_1 = SUV("Toyota","Corolla Cross",23000,5,"4x2",False)
+carro_1.Imprimir()
